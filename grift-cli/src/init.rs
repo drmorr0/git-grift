90033ed3e52e85eb0b4a21b91f6da625ae25abd2
David Morrison <drmorr@evokewonder.com>
2024-08-23 21:22:28 -0700

    cli scaffolding

---
+++ grift-cli/src/init.rs
@@ -0,0 +1,7 @@
+use grift_core::prelude::*;
+
+pub fn cmd() -> Empty {
+    println!("Hello from grift!");
+
+    Ok(())
+}
--- grift-cli/src/main.rs
+++ grift-cli/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+mod completions;
+mod init;
+
+use clap::{
+    crate_version,
+    CommandFactory,
+    Parser,
+    Subcommand,
+};
+use grift_core::prelude::*;
+
+#[derive(Parser)]
+#[command(about = "handy tools for working with git", version, propagate_version = true)]
+
+struct GriftRoot {
+    #[command(subcommand)]
+    subcommand: GriftSubcommand,
+}
+
+#[derive(Subcommand)]
+#[allow(clippy::large_enum_variant)]
+enum GriftSubcommand {
+    #[command(about = "generate shell completions for skctl")]
+    Completions(completions::Args),
+
+    #[command(about = "initialize git grift in this repo")]
+    Init,
+
+    #[command(about = "git-grift version")]
+    Version,
+}
+
+fn main() -> Empty {
+    let args = GriftRoot::parse();
+
+    match &args.subcommand {
+        GriftSubcommand::Completions(args) => completions::cmd(args, GriftRoot::command()),
+        GriftSubcommand::Init => init::cmd(),
+        GriftSubcommand::Version => {
+            println!("git-grift {}", crate_version!());
+            Ok(())
+        },
+    }
 }
